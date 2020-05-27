30bc35db8bca3512efbdc688ff782c1dbf9e62aa
fudesign2008 <fudesign2008@163.com>
2020-05-27 11:22:04 +0800

    new file:   css/app.3216407a.css         new file:   css/chunk-vendors.2ac5db4b.css         new file:   favicon.ico         new file:   fonts/element-icons.535877f5.woff         new file:   fonts/element-icons.732389de.ttf         new file:   img/icons/android-chrome-192x192.png         new file:   img/icons/android-chrome-512x512.png         new file:   img/icons/android-chrome-maskable-192x192.png         new file:   img/icons/android-chrome-maskable-512x512.png         new file:   img/icons/apple-touch-icon-120x120.png         new file:   img/icons/apple-touch-icon-152x152.png         new file:   img/icons/apple-touch-icon-180x180.png         new file:   img/icons/apple-touch-icon-60x60.png         new file:   img/icons/apple-touch-icon-76x76.png         new file:   img/icons/apple-touch-icon.png         new file:   img/icons/favicon-16x16.png         new file:   img/icons/favicon-32x32.png         new file:   img/icons/msapplication-icon-144x144.png         new file:   img/icons/mstile-150x150.png         new file:   img/icons/safari-pinned-tab.svg         new file:   img/wechat.3c57b178.jpg         new file:   index.html         new file:   js/app.020c8ed4.js         new file:   js/app.020c8ed4.js.map         new file:   js/chunk-vendors.caed429d.js         new file:   js/chunk-vendors.caed429d.js.map         new file:   manifest.json         new file:   precache-manifest.e1306d4077b74cf690ed2177cc27b623.js         new file:   robots.txt         new file:   service-worker.js

---
+++ precache-manifest.e1306d4077b74cf690ed2177cc27b623.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bdd8e28acd308e6ee9",
+    "url": "/css/app.3216407a.css"
+  },
+  {
+    "revision": "87b41d4150010ec38b81",
+    "url": "/css/chunk-vendors.2ac5db4b.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3c57b178d8d2a5bbf983e6cc6351539b",
+    "url": "/img/wechat.3c57b178.jpg"
+  },
+  {
+    "revision": "8dfd30287f0a83112298b13f32084a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66bdd8e28acd308e6ee9",
+    "url": "/js/app.020c8ed4.js"
+  },
+  {
+    "revision": "87b41d4150010ec38b81",
+    "url": "/js/chunk-vendors.caed429d.js"
+  },
+  {
+    "revision": "096ac71de9f18d6af4b08338b053f7f1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
