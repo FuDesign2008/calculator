fbae55bc092be14ccc41e07b9889a2fa569d3b7d
fudesign2008 <fudesign2008@163.com>
2020-05-28 09:16:25 +0800

    renamed:    css/app.29b9fffb.css -> css/app.4034b504.css         modified:   index.html         deleted:    js/app.72c5ea53.js         deleted:    js/app.72c5ea53.js.map         new file:   js/app.9fac5420.js         new file:   js/app.9fac5420.js.map         new file:   precache-manifest.0440080a4a99cac63943ff7f03d0f5d3.js         new file:   service-worker.js

---
+++ precache-manifest.0440080a4a99cac63943ff7f03d0f5d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca54e48aaf45652d2437",
+    "url": "/calculator/css/app.4034b504.css"
+  },
+  {
+    "revision": "242522a3b20ab63b6def",
+    "url": "/calculator/css/chunk-vendors.2ac5db4b.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/calculator/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/calculator/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "78daf999edbf376494cf7c5bc6cfccd8",
+    "url": "/calculator/img/wechat.78daf999.jpg"
+  },
+  {
+    "revision": "4f1e6fe6836de23dc66f4d3ff1d3e816",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ca54e48aaf45652d2437",
+    "url": "/calculator/js/app.9fac5420.js"
+  },
+  {
+    "revision": "242522a3b20ab63b6def",
+    "url": "/calculator/js/chunk-vendors.d46c3c85.js"
+  },
+  {
+    "revision": "096ac71de9f18d6af4b08338b053f7f1",
+    "url": "/calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/calculator/robots.txt"
+  }
+]);
